dbae8848da6e7998601b9c8aa817905301980ccb
Borna Vukorepa <36900101+brutalni-vux@users.noreply.github.com>
2022-07-14 17:51:21 +0200

    Update Lemma_5.1_group_Z21.m

--- Q(zeta_7)/Lemma_5.1_group_Z21.m
+++ Q(zeta_7)/Lemma_5.1_group_Z21.m
@@ -1,3 +1,6 @@
+//the curves from isogeny class https://www.lmfdb.org/EllipticCurve/Q/162/b/
+//they are the only j-invariants allowing a rational 21-isogeny
+
 E := EllipticCurve([1, -1, 0, -1077, 13877]);
 p:=DivisionPolynomial(E,7);
 Factorization(ChangeRing(p, CyclotomicField(7)));
